98006c92cfc8433a7a3282f54bdf873ddf462034
keating <mgsong@madup.com>
2021-07-08 08:49:34 +0900

    refactor

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,20 @@
-import words from './words.js'
 import { initAllWords, initDate, initSwiper, getIndex } from './utils.js'
 import dayjs from 'https://unpkg.com/dayjs@1.8.21/esm'
 
 console.log('script.js going..')
 
-window.swiper = new Swiper('.mySwiper', {})
-
 // duration 플러그인 설치
 dayjs.extend(window.dayjs_plugin_duration)
 
+window.swiper = new Swiper('.mySwiper', {})
+
 const today = dayjs()
 
-initSwiper(words, getIndex(today))
+// 말씀 슬라이드 세팅
+initSwiper(today)
 
+// 날짜세팅
 initDate(today)
 
-initAllWords(words)
-
-document
-  .querySelector('footer > details > summary')
-  .addEventListener('click', () => {
-    document.querySelector('section').classList.toggle('hidden')
-    document.querySelector('header').classList.toggle('hidden')
-  })
+// 전체암송구절 세팅
+initAllWords()
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,7 +1,7 @@
 import dayjs from 'https://unpkg.com/dayjs@1.8.21/esm'
 import words from './words.js'
 
-export const initAllWords = words => {
+export const initAllWords = () => {
   document.querySelector('footer > details > ul').innerHTML = words
     .map(word => {
       return `
@@ -16,9 +16,17 @@ export const initAllWords = words => {
     `
     })
     .join('')
+
+  document
+    .querySelector('footer > details > summary')
+    .addEventListener('click', () => {
+      document.querySelector('section').classList.toggle('hidden')
+      document.querySelector('header').classList.toggle('hidden')
+    })
 }
 
-export const initSwiper = (words, currentIndex) => {
+export const initSwiper = today => {
+  const currentIndex = getIndex(today)
   // currentIndex 가 항상 중앙에 위치하게끔 좌우로 더 여유있게 붙여줌
   const after = [...words.slice(currentIndex), ...words.slice(0, currentIndex)]
   const before = [
@@ -27,6 +35,13 @@ export const initSwiper = (words, currentIndex) => {
   ]
   swiper.appendSlide(after.map(wordToHtml))
   swiper.prependSlide(before.map(wordToHtml))
+
+  const initIndex = window.swiper.activeIndex
+
+  swiper.on('slideChange', function () {
+    const diff = swiper.activeIndex - initIndex
+    initDate(today.add(7 * diff, 'day'))
+  })
 }
 
 export const wordToHtml = word => `
