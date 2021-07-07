ab537b13ca782a853e5e9347867c64e7402aee2a
keating <mgsong@madup.com>
2021-07-08 07:24:55 +0900

    refactor: getIndex

--- public/script.js
+++ public/script.js
@@ -6,18 +6,22 @@ console.log('script.js going..')
 // duration 플러그인 설치
 dayjs.extend(window.dayjs_plugin_duration)
 
-// 기준일
-var initDay = dayjs('2021-07-04', 'YYYY-MM-DD')
+const getIndex = () => {
+  // 기준일
+  var initDay = dayjs('2021-07-04', 'YYYY-MM-DD')
 
-// var today = dayjs('2021-07-18', 'YYYY-MM-DD')
-var today = dayjs()
-var weeksFromInitDay = dayjs.duration(today.diff(initDay)).weeks()
-console.log('기준일(2021/7/4)로 부터 이번 주:', weeksFromInitDay)
+  // var today = dayjs('2021-07-18', 'YYYY-MM-DD')
+  var today = dayjs()
+  var weeksFromInitDay = dayjs.duration(today.diff(initDay)).weeks()
+  console.log('기준일(2021/7/4)로 부터 이번 주:', weeksFromInitDay)
 
-var start = today.startOf('week').format('YYYY.M.D')
-var end = today.endOf('week').format('M.D')
+  var start = today.startOf('week').format('YYYY.M.D')
+  var end = today.endOf('week').format('M.D')
 
-var currentIndex = weeksFromInitDay % words.length
+  return weeksFromInitDay % words.length
+}
+
+const currentIndex = getIndex()
 
 const wordToHtml = word => `
     <div class="swiper-slide"><section>
