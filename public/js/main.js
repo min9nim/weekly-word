2b08157f943c696d4bd8fed760eb6e45d6194c55
keating <mgsong@madup.com>
2021-07-26 21:10:46 +0900

    remove log

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 import { initApp } from './utils.js'
 import dayjs from 'https://unpkg.com/dayjs@1.8.21/esm'
 
-console.log('main.js start')
-
 // duration 플러그인 설치
 dayjs.extend(window.dayjs_plugin_duration)
 
